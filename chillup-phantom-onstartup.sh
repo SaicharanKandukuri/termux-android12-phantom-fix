5325208d1f827f911eba0984263f8f2ce70ac2cd
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-04-12 20:13:58 +0530

    remove hardcoded device name

--- chillup-phantom-onstartup.sh
+++ chillup-phantom-onstartup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cat <<- EOF >> ~/.bashrc
-cur_phantom_peak=\$(adb -s localhost:55557 shell device_config get activity_manager max_phantom_processes)
+cat <<-EOF >>~/.bashrc
+cur_phantom_peak=\$(adb   shell device_config get activity_manager max_phantom_processes)
 
 if [[ "\$cur_phantom_peak" == 'null' ]]; then
     echo "fixing phantom.."
-    adb -s localhost:55557 \\
+    adb   \\
         shell \\
         device_config \\
         put activity_manager \\
@@ -13,7 +13,7 @@ if [[ "\$cur_phantom_peak" == 'null' ]]; then
     echo "process limit increased from \$cur_phantom_peak to 214181594"
 fi
 
-if [ "\$(adb -s localhost:55557 shell device_config get activity_manager max_phantom_processes)" == 'null' ]; then
+if [ "\$(adb   shell device_config get activity_manager max_phantom_processes)" == 'null' ]; then
     echo "Failed to increase phantom max_phantom_processes"
     echo "check returned null"
 fi
--- initadb.sh
+++ initadb.sh
@@ -6,12 +6,31 @@ _c_red="\e[31m"
 _c_blue="\e[34m"
 RST="\e[0m"
 
-die()    { echo -e "${_c_red}[E] ${*}${RST}";exit 1;:;}
-warn()   { echo -e "${_c_red}[W] ${*}${RST}";:;}
-shout()  { echo -e "${_c_blue}[-] ${*}${RST}";:;}
-lshout() { echo -e "${_c_blue}-> ${*}${RST}";:;}
-msg()    { echo -e "${*} \e[0m" >&2;:;}
-success() { echo -e "${_c_green}${*} \e[0m" >&2;:;}
+die() {
+    echo -e "${_c_red}[E] ${*}${RST}"
+    exit 1
+    :
+}
+warn() {
+    echo -e "${_c_red}[W] ${*}${RST}"
+    :
+}
+shout() {
+    echo -e "${_c_blue}[-] ${*}${RST}"
+    :
+}
+lshout() {
+    echo -e "${_c_blue}-> ${*}${RST}"
+    :
+}
+msg() {
+    echo -e "${*} \e[0m" >&2
+    :
+}
+success() {
+    echo -e "${_c_green}${*} \e[0m" >&2
+    :
+}
 
 echo "Enter Auth port ( Port show in option enter six digit number): "
 read -r authport
@@ -23,7 +42,7 @@ read -r debugport
 
 shout "Trying to pair"
 adb pair localhost:$authport $authpincode || {
-    die "Connection Failed?"    
+    die "Connection Failed?"
 }
 success "Pairing localhost:$authport succeed.."
 
@@ -41,12 +60,11 @@ adb devices -l
 success "ADB setup complete..."
 
 shout "Trying to fix phantom service for this session.."
-adb -s localhost:55557 \
-        shell \
-        device_config \
-        put activity_manager \
-        max_phantom_processes 214181594 || {
-            lwarn "Failed to set max_phantom_processes"
-        }
-success "current max_phantom_processes = $(adb -s localhost:55557 shell device_config get activity_manager max_phantom_processes)"
-
+adb \
+    shell \
+    device_config \
+    put activity_manager \
+    max_phantom_processes 214181594 || {
+    lwarn "Failed to set max_phantom_processes"
+}
+success "current max_phantom_processes = $(adb shell device_config get activity_manager max_phantom_processes)"
